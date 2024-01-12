8e5fa44bf3aa8f4c9f52e0453d1c4b1b870d8870
m-bleasdale <90987696+m-bleasdale@users.noreply.github.com>
2024-01-12 23:04:09 +0000

    Cleaned up commented-out code

--- Server/server.js
+++ Server/server.js
@@ -10,31 +10,3 @@ mountRoutes(app);
 
 app.listen(port_dev, () => console.log(`Carshare backend API listening at http://localhost:${port_dev}`));
 
-// Endpoint: Read vehicle by ID
-//app.get('/vehicle/:id', (req, res) => {
-
-    
-
-
-    /*
-    client.query('SELECT $1::text as message', ['Hello world!'])
-        .then((res) => {
-            console.log(res.rows[0].message);
-        })
-        .then(() => {
-            client.end();
-        })
-
-
-    const vehicleReference = db.collection('vehicles').doc(req.params.id);
-    vehicleReference.get().then((vehicle) => {
-        if (!vehicle.exists){
-            res.status(404).send('Vehicle not found');
-        }
-        else{
-            res.json(vehicle);
-        }
-    });
-    */
-//});
-
